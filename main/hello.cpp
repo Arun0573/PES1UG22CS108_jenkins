0a71dc014bd967d5b37e30fbf7388c645cf68b6b
Arun Kumar R <111110702+Arun0573@users.noreply.github.com>
2025-03-18 15:12:25 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,13 @@
-//File: hello.cpp
-#include <iostream>
-using namespace std;
+// File: hello.cpp
+#include <iostream>  // Include the iostream library for input/output
 
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  return 0;
+using namespace std; // Use the standard namespace
+
+int main() {
+    cout << "Hello, World\n";
+    cout << "Hello, Jenkins\n";
+    cout << "I have successfully built and run this.\n";
+    cout << "PES1UG22CS108\n";
+    cout << "Name: Arun Kumar R\n";  // Space added for better readability
+    return 0;  // Return 0 to indicate successful execution
 }
